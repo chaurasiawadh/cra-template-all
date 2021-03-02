68041a3d4bcab53729fccfde7e52e68a92f04141
Awadhesh Chaurasia <awadhesh.chaurasia@mediassist.in>
2021-03-02 18:52:30 +0530

    add package js-util-methods

--- template/src/modules/home/Home.tsx
+++ template/src/modules/home/Home.tsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGetApiCallExample, fetchPostApiCallExample } from '../../store/actions/exampleAction';
+import { getPercentage, formatPriceINR } from 'js-util-methods';
 
 const HomeComponent = () => {
     const dispatch = useDispatch();
@@ -20,6 +21,9 @@ const HomeComponent = () => {
 
     return (
         <div>
+            <h1><a href="https://www.npmjs.com/package/js-util-methods">Using JS-Utility </a> </h1>
+            <h6>getPercentage(2000, 200): {getPercentage(2000, 200)}% </h6>
+            <h6>formatPriceINR(2076200): {formatPriceINR(2076200)} </h6>
             <h1>This is Home Component</h1>
             <button onClick={getAPICall} style={{ marginRight: 10 }}>GET API CALL</button>
             <button onClick={postAPICall}>POST API CALL</button>
